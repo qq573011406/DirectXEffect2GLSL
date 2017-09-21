b6bd4951013acb81c73a37f91310a7c43895be05
袁全伟 <qq573011406@126.com>
2017-09-21 12:57:09 +0800

    编译参数

--- Platform/main.cpp
+++ Platform/main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include "hlsl2glsl.h"
 
 int main(int argc,char** argv)
 {
+	ShHandle parser = Hlsl2Glsl_ConstructCompiler(EShLangVertex);
     std::cout<<"hello world!"<<std::endl;
     return 0;
 }
